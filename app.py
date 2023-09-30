11b7817ff07ecca1abd4e1c5673b2a9e2b0f8623
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2023-10-01 03:17:30 +0530

    completed project

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+from util import classify
+
+# set title
+st.title('Elephant classification')
+
+# set header
+st.header('Please upload an Elephant Image')
+
+# upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# load classifier
+model = load_model('./elephant_prediction.h5')
+
+# load class names
+with open('./labels.txt', 'r') as f:
+    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+    f.close()
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    # classify image
+    class_name, conf_score = classify(image, model, class_names)
+
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
